df5ec448c965e0b04ae365dc23ac190350ba9fe0
islamovic <islam.abo.elsoud@gmail.com>
2020-01-22 00:45:20 +0200

    init commit

---
+++ Movies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Movies
+//
+//  Created by Islam on 1/22/20.
+//  Copyright © 2020 App Lineup. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
